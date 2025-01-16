d8acab6689d8a62874abef3359f9478d69931655
erwne <erwan.nea@ynov.com>
2025-01-16 14:56:16 +0100

    Mise en place du swagger

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const config = new DocumentBuilder()
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.enableCors({
-    origin: 'http://localhost:5173', 
-    methods: 'GET,POST,PUT,DELETE,OPTIONS', 
-    credentials: true, 
-    
+    origin: 'http://localhost:5173',
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+    credentials: true,
+
   });
 
-  await app.listen(process.env.PORT ?? 3000);
-  console.log(`Backend running on http://localhost:${process.env.PORT ?? 3000}`);
+  await app.listen(3000);
 }
 bootstrap();

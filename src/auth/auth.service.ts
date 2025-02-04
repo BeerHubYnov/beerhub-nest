436bf929124756e151e1df009de3a51c82423e6a
Quidi boz <boireauguy@gmail.com>
2025-02-04 14:04:19 +0100

    pwd hash

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -19,32 +19,19 @@ export class AuthService {
     private readonly jwtService: JwtService,
   ) {}
 
-  async validateUser(
-    username: string,
-    pass: string,
-  ): Promise<CreateUserDto | null> {
-    const user = await this.userService.findByUsername(username);
-    if (user && (await bcrypt.compare(pass, user.password))) {
-      const { password, ...result } = user; // eslint-disable-line @typescript-eslint/no-unused-vars
-      return {
-        userId: Number(user.id),
-        email: user.email,
-        username: user.username,
-        password: user.password,
-      } as unknown as CreateUserDto;
-    }
-    return null;
-  }
-
   async login(user: LoginDto): Promise<{ access_token: string }> {
     const foundUser = await this.userService.findByUsername(user.username);
     if (!foundUser) {
       throw new NotFoundException('User not found');
     }
-    if (foundUser.password !== user.password) {
+    const isPasswordValid = await bcrypt.compare(
+      user.password,
+      foundUser.password,
+    );
+    if (!isPasswordValid) {
       throw new UnauthorizedException('Incorrect password');
     }
-    const payload = { username: user.username, sub: foundUser.id };
+    const payload = { username: user.username, userId: foundUser.id };
     return {
       access_token: this.jwtService.sign(payload),
     };
@@ -71,9 +58,5 @@ export class AuthService {
         username: createdUser.username,
       } as CreateUserDto,
     };
-  }
-
-  async logout(token: string): Promise<void> {
-    this.revokedTokens.add(token);
   }
 }
